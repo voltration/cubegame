8b316ca768c66c578e6a85a9a49707d7c5b5c2b7
Sander <sqndxr@gmail.com>
2024-03-24 01:34:07 +0100

    dot

--- src/lib/components/TutorialButton.svelte
+++ src/lib/components/TutorialButton.svelte
@@ -9,7 +9,7 @@
         <div class="pt-4">
             <p class="text-white text-xl i700">These are the following rules:</p>
             <div class="pt-2">
-                <p class="text-white text-xl i500">• You have two grids, and both grids have a player with share the same input</p>
+                <p class="text-white text-xl i500">• You have two grids, and both grids have a player with share the same input.</p>
                 <p class="text-white text-xl i500">• Have both players at the green grid at the same time to win.</p>
                 <p class="text-white text-xl i500">• Players are not able to walk into the red one.</p>
                 <p class="text-white text-xl i500">• Use WASD or your arrow keys to move.</p>

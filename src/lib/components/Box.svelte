0fa2675c899936839ea8dfc3a07a7c642bcf61d6
voltration <sqndxr@gmail.com>
2024-05-28 11:04:07 +0200

    mmm

--- src/lib/components/Box.svelte
+++ src/lib/components/Box.svelte
@@ -9,23 +9,23 @@
 
     let div: HTMLDivElement;
 
-    const colorWays: { [key: string]: { background: string, border: string } } = {
-        "N": { background: "bg-slate-500", border: "border-slate-600" },
-        "G": { background: "bg-[#439d75]", border: "border-[#285e46]" },
-        "R": { background: "bg-[#aa5c68]", border: "border-[#774049]" }
+    const colorWays: { [key: string]: { background: string, border: string, shadow: string } } = {
+        "N": { background: "bg-slate-500", border: "border-slate-600", shadow: "shadow-slate-600/50" },
+        "G": { background: "bg-[#439d75]", border: "border-[#285e46]", shadow: "shadow-[#285e46]/50" },
+        "R": { background: "bg-[#aa5c68]", border: "border-[#774049]", shadow: "shadow-[#774049]/50" }
     };
 
     const colorWay = colorWays[array[index]];
 
     onMount(() => {
         if (colorWay) {
-            div.classList.add(colorWay.background, colorWay.border);
+            div.classList.add(colorWay.background, colorWay.border, colorWay.shadow);
         }
     });
 </script>
 
 <body>
-    <div bind:this={div} class="size-16 md:size-20 border-b-8 rounded-md flex items-center justify-center">
+    <div bind:this={div} class="size-16 md:size-20 shadow-lg border-b-4 rounded-md flex items-center justify-center">
         {#if playerIndex == index}
             <Player />
         {/if}
--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -3,6 +3,7 @@
     import { levels } from "$lib/Levels";
     import GameIterator from "./GameIterator.svelte";
     import { getMiddleIndex } from "$lib/arrayHelper";
+	import { confetti } from "@neoconfetti/svelte";
 
     let left: string[];
     let right: string[];
@@ -30,33 +31,28 @@
     }
 </script>
 
-<div class="flex flex-col gap-8 h-screen items-center mt-4">
-    <div class="flex flex-col gap-4">
-        <a href="/" class="i100 text-center  text-white text-6xl">cube<span class="text-blue-500">game</span></a>
-        <div class="flex flex-row w-full justify-between gap-2">
-            <p class="i400 text-white/90 rounded-md border-2 border-blue-500/20 bg-blue-500/10 shadow-lg shadow-blue-950/30 px-4 py-1">Level {$currentLevel}</p>
-            <p class="i400 text-white/90 rounded-md border-2 border-blue-500/20 bg-blue-500/10 shadow-lg shadow-blue-950/30 px-4 py-1">{levelsLeft} levels left</p>
-            <a class="i400 text-white/90 rounded-md border-2 border-blue-500/30 bg-blue-500/30 shadow-lg shadow-blue-950/30 px-4 py-1" href="/tutorial">Tutorial</a>
-        </div>
-    </div>
-    <div class="flex flex-col gap-4 md:flex-row md:gap-24">
-        {#key $currentLevel}
-            {#if left}
-                <GameIterator side="left" index={getMiddleIndex(left)} array={left} />
-            {/if}
-            <div class="h-[1px] w-full bg-white/5 md:hidden"></div>
-            {#if right}
-                <GameIterator side="right" index={getMiddleIndex(right)} array={right} />
-            {/if}
-        {/key}
-    </div>
-</div>
-<!-- 
-<div class="flex items-center justify-center h-screen overflow-hidden">
+<div class="flex flex-col gap-8 h-screen items-center pt-4">
     {#if completed}
-        <div class="absolute top-0">
-            <div class="overflow-hidden" use:confetti></div>
+        <div class="flex flex-col gap-4">
+            <a href="/" class="i100 text-center  text-white text-6xl">cube<span class="text-blue-500">game</span></a>
+            <div class="flex flex-row w-full justify-between gap-2">
+                <p class="i400 text-white/90 rounded-md border-2 border-blue-500/20 bg-blue-500/10 shadow-lg shadow-blue-950/30 px-4 py-1">Level {$currentLevel}</p>
+                <p class="i400 text-white/90 rounded-md border-2 border-blue-500/20 bg-blue-500/10 shadow-lg shadow-blue-950/30 px-4 py-1">{levelsLeft} levels left</p>
+                <a class="i400 text-white/90 rounded-md border-2 border-blue-500/30 bg-blue-500/30 shadow-lg shadow-blue-950/30 px-4 py-1" href="/tutorial">Tutorial</a>
+            </div>
         </div>
+        <div class="flex flex-col gap-4 md:flex-row md:gap-24">
+            {#key $currentLevel}
+                {#if left}
+                    <GameIterator side="left" index={getMiddleIndex(left)} array={left} />
+                {/if}
+                <div class="h-[1px] w-full bg-white/5 md:hidden"></div>
+                {#if right}
+                    <GameIterator side="right" index={getMiddleIndex(right)} array={right} />
+                {/if}
+            {/key}
+        </div>
+    {:else}
         <div class="flex flex-col">
             <a on:click={() => $currentLevel = 1} href="/" class="i100 text-center  text-white text-[4rem]">cube<span class="text-blue-500">game</span></a>
             <p class="text-white i400 text-center">No more levels left... You won!</p>
@@ -64,34 +60,8 @@
                 <button>Reset level count?</button>
             </div>
         </div>
-    {:else}
-        <div class="flex flex-col gap-12">
-            <div class="flex flex-col w-full items-center gap-2">
-                <a href="/" class="i100 text-center  text-white text-[4rem]">cube<span class="text-blue-500">game</span></a>
-                <div class="flex flex-row gap-4">
-                    <div class="rounded-md border-2 border-blue-500/20 shadow-lg shadow-blue-950/30 p-2 px-6">
-                        <p class="i400 text text-white">Level {$currentLevel}</p>
-                    </div>
-                    <div class="rounded-md border-2 border-blue-500/20 shadow-lg shadow-blue-950/30 p-2 px-6">
-                        <p class="i400 text text-white">{levelsLeft} levels left</p>
-                    </div>
-                    <TutorialButton />
-                    {#if dev}
-                        <input bind:value={$currentLevel} class="border-[1px] rounded-md border-blue-500/30 border-white bg-transparent text-white p-2">
-                    {/if}
-                </div>
-            </div>
-            <div class="flex justify-between gap-24">
-                {#key $currentLevel}
-                    {#if left}
-                        <GameIterator side="left" index={getMiddleIndex(left)} array={left} />
-                    {/if}
-                    {#if right}
-                        <GameIterator side="right" index={getMiddleIndex(right)} array={right} />
-                    {/if}
-                {/key}
-            </div>
+        <div class="absolute top-0">
+            <div class="overflow-hidden" use:confetti></div>
         </div>
     {/if}
 </div>
--->
